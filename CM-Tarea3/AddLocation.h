bc8768c86e6d9c2e999ee47bb883579901c1e5c6
Vita V <dj.vita.09@gmail.com>
2015-07-05 13:32:54 -0500

    Added Classes for addloc

---
+++ CM-Tarea3/AddLocation.h
@@ -0,0 +1,13 @@
+//
+//  AddLocation.h
+//  CM-Tarea3
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 7/5/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddLocation : UIViewController
+
+@end
---
+++ CM-Tarea3/AddLocation.m
@@ -0,0 +1,37 @@
+//
+//  AddLocation.m
+//  CM-Tarea3
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 7/5/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import "AddLocation.h"
+
+@interface AddLocation ()
+
+@end
+
+@implementation AddLocation
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
