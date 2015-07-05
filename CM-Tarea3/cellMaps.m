3c1aa506ad188d2c883f2d70d5589af8c7f1718e
Vita V <dj.vita.09@gmail.com>
2015-07-05 13:31:20 -0500

    Added Elements

---
+++ CM-Tarea3/cellMaps.m
@@ -0,0 +1,23 @@
+//
+//  cellMaps.m
+//  CM-Tarea3
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 7/5/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import "cellMaps.h"
+
+@implementation cellMaps
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
