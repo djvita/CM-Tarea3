7844bb53bf30348147ef925d77a0887f931e53b4
Vita V <dj.vita.09@gmail.com>
2015-07-05 21:38:42 -0500

    Added declarations class to save location and coordinates

---
+++ CM-Tarea3/Declarations.h
@@ -0,0 +1,13 @@
+//
+//  Declarations.h
+//  CM-Tarea3
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 7/5/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Declarations : NSObject
+
+@end
---
+++ CM-Tarea3/Declarations.m
@@ -0,0 +1,13 @@
+//
+//  Declarations.m
+//  CM-Tarea3
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 7/5/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import "Declarations.h"
+
+@implementation Declarations
+
+@end
